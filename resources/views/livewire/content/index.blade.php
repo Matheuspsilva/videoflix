845cd750d9f67d28ff40154bc76c37f6518d9163
Matheus <matheuspsilva29@gmail.com>
2022-01-03 17:24:06 -0300

    Add content remove

--- resources/views/livewire/content/index.blade.php
+++ resources/views/livewire/content/index.blade.php
@@ -1,5 +1,14 @@
 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
     <x-slot name="header">Conteúdos cadastrados</x-slot>
+
+    <a href="{{route('content.create')}}">Criar conteúdo</a>
+
+    @if (session()->has('success'))
+        <div class="w-full p-5 border border-green-500 bg-green-400">
+            {{ session('success') }}
+        </div>
+    @endif
+
     @foreach ($contents as $content )
         {{-- @livewire('content.content', ['content' => $content], key($content->id)) --}}
         <livewire:content.content :content="$content" :key="$content->id"  ></livewire:content.content>
